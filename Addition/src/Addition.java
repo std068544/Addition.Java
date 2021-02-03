840c1e1356c14875ef4f105b315ceccc8ef0c27a
Vassilis <Vassilis@Vassis-Desktop>
2021-02-03 23:18:47 +0200

    Create Addition

---
+++ Addition/src/Addition.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Πρόγραμμα πρόσθεσης που χρησιμοποιεί το JOptionPane για είσοδο και έξοδο
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Vassilis Dionisopoulos
+ */
+public class Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // παίρνει είσοδο από τον χρήστη από παράθυρο διαλόγου εισόδου JOptionPane
+        String firstNumber = JOptionPane.showInputDialog("Enter first integer: ");
+        String secondNumber = JOptionPane.showInputDialog("Enter second integer: ");
+        
+    }
+    
+}
